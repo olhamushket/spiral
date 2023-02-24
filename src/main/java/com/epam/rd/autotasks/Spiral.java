afb74fa3d6ca984cc415d8ca638b58959f41e8ab
Olha Mushket <olhamushket@gmail.com>
2023-02-25 00:08:57 +0200

    Solved

--- src/main/java/com/epam/rd/autotasks/Spiral.java
+++ src/main/java/com/epam/rd/autotasks/Spiral.java
@@ -8,43 +8,31 @@ class Spiral {
         int minC=0;
         int maxC=columns-1;
         int value=1;
-        while (maxR>0&&maxC>0) {
+        while (maxR>=0&&maxC>=0) {
                 for (int i = minC; i <= maxC; i++) {
-                    System.out.println(value);
                     if (value>rows*columns) return spi;
                     spi[minR][i] = value;
                     value++;
-                    System.out.println(value);
                 }
                     minR++;
-                System.out.println("minR="+minR);
                 for (int i = minR; i <= maxR; i++) {
-                    System.out.println(value);
                     if (value>rows*columns) return spi;
                     spi[i][maxC] = value;
                     value++;
-                    System.out.println(value);
                 }
                     maxC--;
-                System.out.println("maxC="+maxC);
                 for (int i = maxC; i >= minC; i--) {
-                    System.out.println(value);
                     if (value>rows*columns) return spi;
                     spi[maxR][i] = value;
                     value++;
-                    System.out.println(value);
                 }
                     maxR--;
-                System.out.println("maxR="+maxR);
                 for (int i = maxR; i >= minR; i--) {
-                    System.out.println(value);
                     if (value>rows*columns) return spi;
                     spi[i][minC] = value;
                     value++;
-                    System.out.println(value);
                 }
                     minC++;
-                System.out.println("minC="+minC);
             }
         return spi;
     }
